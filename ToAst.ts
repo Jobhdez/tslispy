4041fdd7b56aec20d5ac1e17db42e9d57d497682
Job Hernandez <hj93@protonmail.com>
2024-08-28 12:03:56 -0700

    remove comments

--- ToAst.ts
+++ ToAst.ts
@@ -4,7 +4,6 @@ import { SchemeParser, ExpLetContext, ExpContext, BindingContext } from './Schem
 import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor';
 import { SchemeVisitor } from './SchemeVisitor';
 
-// 1. AST Node Definition
 class LetExpressionAST {
   bindings: { variable: string; value: any }[];
   body: any;
@@ -15,7 +14,6 @@ class LetExpressionAST {
   }
 }
 
-// 2. Visitor Implementation
 class BuildASTVisitor extends AbstractParseTreeVisitor<any> implements SchemeVisitor<any> {
   defaultResult() {
     return null;
@@ -33,7 +31,7 @@ class BuildASTVisitor extends AbstractParseTreeVisitor<any> implements SchemeVis
 
     for (const binding of bindingList) {
       bindings.push({
-        variable: binding.VAR().text, // Access the variable name using VAR()
+        variable: binding.VAR().text,
         value: this.visit(binding.exp())
       });
     }
@@ -52,26 +50,26 @@ class BuildASTVisitor extends AbstractParseTreeVisitor<any> implements SchemeVis
   }
 }
 
-// 3. Usage: Parsing and Generating the AST
-// Example Scheme expression
+
+
 const input = '(let ((n 2)) n)';
 
-// Create an input stream from the Scheme expression
+
 const inputStream = new ANTLRInputStream(input);
 
-// Create a lexer that feeds off of the input stream
+
 const lexer = new SchemeLexer(inputStream);
 
-// Create a token stream from the lexer
+
 const tokenStream = new CommonTokenStream(lexer);
 
-// Create a parser that feeds off of the token stream
-const parser = new SchemeParser(tokenStream);
-const tree = parser.prog(); // Assume 'prog' is the start rule
 
-// Create the visitor
+const parser = new SchemeParser(tokenStream);
+const tree = parser.prog(); 
+
+
 const buildASTVisitor = new BuildASTVisitor();
-// Generate the AST by visiting the parse tree
+
 const ast = buildASTVisitor.visit(tree);
 
 console.log(ast instanceof LetExpressionAST)
